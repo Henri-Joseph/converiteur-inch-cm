e0fb4b2d297dc76685f96c337ad06e749ba3c64a
Henri-Joseph <joh@bassahenrij.com>
2023-07-27 13:30:24 +0000

    Convertion avec validation de saisie

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-# Initialisation des valeurs par default (pouce - centimetre / centimetre - pouce)
+# Initialisation des valeurs par default (pouce - centimetre / centimetre - pouce) - ETAPE 1
 POUCE = 2.54
 CM = 0.394
 
-# Choix de l'opération a effectué
+# Choix de l'opération a effectué - ETAPE 2
 def demande_convertion():
     print("1 - Convertion de pouce à cm | 2 - Convertion de cm à pouce")
     print()
@@ -16,16 +16,59 @@ def demande_convertion():
     return choix_int
 
 
-# Verification de l'instruction demandée
+# Converstion de pouce à cm avec les contraintes - ETAPE 4a
+def convertion_un():
+    valPouceFloat = "x"
+    # Verification du type de la vaariable
+    while valPouceFloat == "x":
+        valPouce = input("Entrer la valeur (Entier/Réel) à convertir de pouce à cm ")
+        try:
+            valPouceFloat = float(valPouce)
+        except ValueError:
+            print("Respectez l'instruction ci-dessous")
+    # Validation du type de la variable et convertion
+    x = isinstance(valPouceFloat, (float, int))
+    if x:
+        valPoucetoCm = valPouceFloat * POUCE
+    else:
+        valPoucetoCm = "x"
+    return valPoucetoCm
+
+# Converstion de pouce à cm avec les contraintes - ETAPE 4b
+def convertion_deux():
+    valPouceFloatD = "y"
+    # Verification du type de la vaariable
+    while valPouceFloatD == "y":
+        valPouceD = input("Entrer la valeur (Entier/Réel) à convertir de cm à pouce ")
+        try:
+            valPouceFloatD = float(valPouceD)
+        except ValueError:
+            print("Respectez l'instruction ci-dessous")
+    # Validation du type de la variable et convertion
+    x = isinstance(valPouceFloatD, (float, int))
+    if x:
+        valCmtoPouce = valPouceFloatD * CM
+    else:
+        valCmtoPouce = "y"
+    return valCmtoPouce
+
+
+# Verification de l'instruction demandée - ETAPE 3
 vChoix = 0
 while not vChoix == 1 and not vChoix == 2:
     vChoix = demande_convertion()
 
-
+# Convertion selon le mode choisi
 if vChoix == 1:
-    print("Convertion de pouce à cm")
+    resulCon = "x"
+    while resulCon == "x":
+        resulCon = convertion_un()
+    print(f"Le resultat de la convetion est {resulCon} Cm")
 elif vChoix == 2:
-    print("Convertion de cm à pouce")
+    resulConD = "y"
+    while resulConD == "y":
+        resulConD = convertion_deux()
+    print(f"Le resultat de la convertion est {resulConD} Inch")
 else:
     print("Respecter les consignes suivantes")
 
